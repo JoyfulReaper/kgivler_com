9a4f1e504bcb90cc6292358f6acda8975bc6db01
Kyle Givler <xvegan88x@gmail.com>
2026-06-21 20:12:35 -0400

    CLEAN SLATE

--- Kgivler_com/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace kgivler_com.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-kgivler_com
+# Personal Website of Kyle Givler
+
+The source code for kgivler.com
+
+Licensed MIT
+Compyright 2026 Kyle Givler
